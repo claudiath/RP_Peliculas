ed9fa4aea085865d89d4471608470da2ec56d730
Claudia Torres Hernández <claudiatorreshernandez88@gmail.com>
2021-02-16 20:06:24 +0100

    Subiendo carpeta Modelos, clase Pelicula y sus atributos.

---
+++ RP_Peliculas/Modelos/Pelicula.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RP_Peliculas.Modelos
+{
+    public class Pelicula
+    {
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public string Descripcion { get; set; }
+    public decimal Precio { get; set; }
+    public DateTime FechaLanzamiento { get; set; }
+    }
+}
