aa3e3ea0104abc2317b3a0e0893796e978c9f5fe
Claudia Torres Hernández <claudiatorreshernandez88@gmail.com>
2021-02-17 21:34:31 +0100

    Subiendo filtro por Título de Película.

--- RP_Peliculas/Modelos/Pelicula.cs
+++ RP_Peliculas/Modelos/Pelicula.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,9 +13,13 @@ namespace RP_Peliculas.Modelos
     public string Titulo { get; set; }
     public string Genero { get; set; }
     public string Descripcion { get; set; }
-    public decimal Precio { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")] // atributo para añadir decimales
+        public decimal Precio { get; set; }
 
     [DataType(DataType.Date)] //va mostrar solo la fecha en la página web creada
-    public DateTime FechaLanzamiento { get; set; }
+
+    [Display(Name = "Fecha de Lanzamiento")] // atributo para cambiar el nombre del campo que se le muestra al usuario
+        public DateTime FechaLanzamiento { get; set; }
     }
 }
--- RP_Peliculas/Pages/Peliculas/Index.cshtml.cs
+++ RP_Peliculas/Pages/Peliculas/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RP_Peliculas.Data;
 using RP_Peliculas.Modelos;
@@ -18,12 +19,23 @@ namespace RP_Peliculas.Pages.Peliculas
         {
             _context = context;
         }
-
         public IList<Pelicula> Pelicula { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string searchString { get; set; } //variable que se va a utilizar para guardar lo que el usuario ponga en el buscador
+        public SelectList genero { get; set; } //lista con generos
+        [BindProperty(SupportsGet = true)]
+        public string generoPelicula { get; set; } //va ser el valor que elija el usuario al seleccionar un genero de la lista
 
         public async Task OnGetAsync()
         {
-            Pelicula = await _context.Pelicula.ToListAsync();
+            var pelicula = from n in _context.Pelicula
+                           select n;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                pelicula = pelicula.Where(S => S.Titulo.Contains(searchString));
+            }
+            Pelicula = await pelicula.ToListAsync(); // para mostrar lo que se filtra en la variable que hemos creado, si no se filtra, se muestra todo
+            //Pelicula = await _context.Pelicula.ToListAsync();
         }
     }
 }
